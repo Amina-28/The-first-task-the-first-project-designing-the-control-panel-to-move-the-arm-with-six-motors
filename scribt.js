a61f50dd07347223f8531eacf199ea3879a88997
Amina-28 <85812076+Amina-28@users.noreply.github.com>
2021-06-26 23:35:13 +0300

    Add files via upload

---
+++ scribt.js
@@ -0,0 +1,58 @@
+
+var slider = document.getElementById("Range1");
+var output = document.getElementById("range-slider__value1");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+  var slider2 = document.getElementById("Range2");
+  var output2 = document.getElementById("range-slider__value2");
+  output2.innerHTML = slider2.value;
+  
+  slider2.oninput = function() {
+  output2.innerHTML = this.value;
+  }
+
+  var slider3 = document.getElementById("Range3");
+  var output3 = document.getElementById("range-slider__value3");
+  output3.innerHTML = slider3.value;
+  
+  slider3.oninput = function() {
+    output3.innerHTML = this.value;
+  
+  }
+  var slider4 = document.getElementById("Range4");
+  var output4 = document.getElementById("range-slider__value4");
+  output4.innerHTML = slider4.value;
+  
+  slider4.oninput = function() {
+    output4.innerHTML = this.value;
+  
+  }
+
+
+  
+  var slider5 = document.getElementById("Range5");
+  var output5 = document.getElementById("range-slider__value5");
+  output5.innerHTML = slider5.value;
+  
+  slider5.oninput = function() {
+    output5.innerHTML = this.value;
+  
+  }
+  
+  var slider6 = document.getElementById("Range6");
+  var output6 = document.getElementById("range-slider__value6");
+  output6.innerHTML = slider6.value;
+  
+  slider6.oninput = function() {
+    output6.innerHTML = this.value;
+  
+  }
+
+  
+  
+ 
